d9a3fd39e04f907d3fd5241af1edad0ce9676dd4
Unknown <rock.droid8@gmail.com>
2017-10-23 20:54:45 -0500

    1

---
+++ learning1/src/components/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import UserList from '../components/UserList';
+import UserForm from '../components/UserForm';
+import logo from '../assets/logo.svg';
+import '../assets/App.css';
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            users: [
+                {id: 1, name: "miguel", email: "miguel@miguel.com"},
+                {id: 2, name: "test", email: "test@test.com"}
+            ]
+        };
+    }
+    
+    handleOnAddUser (event){
+        event.preventDefault();
+        let user = {
+            name: event.target.name.value,
+            email: event.target.email.value
+        };
+        this.setState({
+            users: this.state.users.concat([user])
+        });
+    }
+    
+    render(){
+        console.log(this.state.users);
+        return(
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Bienvenido a React </h2>
+                </div>
+                <div>
+                    <p><strong>Añade usuarios</strong></p>
+                    <UserList users={this.state.users} />
+                    <UserForm onAddUser={this.handleOnAddUser.bind(this)} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
